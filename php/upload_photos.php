4cfdd6c4457594791096f7745d08e8a6a3e543ba
ElizabethMartinovskaya <martinovskaya6@gmail.com>
2017-05-28 17:29:15 +0300

    Добавление изображений

--- php/upload_photos.php
+++ php/upload_photos.php
@@ -6,7 +6,7 @@ $name = basename($_FILES['Image_src']['name']);
 $tmp_name = $_FILES['Image_src']['tmp_name'];
 $type_file = str_replace('image/','.',$_FILES['Image_src']['type']);
 $upload_dir = '../images/';
-$upload_file = $upload_dir.$name;
+$upload_file = $upload_dir;
 $valid_types = array('image/jpeg','image/jpg');
 $image_info = getimagesize($tmp_name);
 $max_size = 10485760;
@@ -18,10 +18,11 @@ function Add_photo($image_info,$name_file,$tmp_name_file,$type_file,$upload_dir,
     $id_properties = Get_Id_Properties_Image($width_image,$height_image);
     $id_event = Get_Id_Event($Source,$type_data);
     $add_update = sql_update("INSERT INTO `image` (`Image_src`,`id_author`,`description_image`,`id_properties`,`id_event`,`publication_date`,`id_category_image`,`image_N`, `image_E`, `isConfirmed`) 
-                              VALUES ('{$tmp_name_file}','{$id_author}','{$description_image}','{$id_properties}','{$id_event}','{$now_date}','','{$coordinates[0]}', '{$coordinates[1]}'), '1'");
+                              VALUES ('{$tmp_name_file}','{$id_author}','{$description_image}','{$id_properties}','{$id_event}','{$now_date}',NULL ,'{$coordinates[0]}', '{$coordinates[1]}', '1')");
     $id_upload_file = sql_select("SELECT `id_image` FROM `image` WHERE `image`.`Image_src`='{$tmp_name_file}'");
-    $Image_src = $upload_dir.$id_upload_file.$type_file;
-    $update_src = sql_update("UPDATE `image` SET `Image_src` = '{$Image_src}', 
+    $id_upload_file = mysqli_fetch_assoc($id_upload_file)['id_image'];
+    $Image_src = "images/".$id_upload_file.$type_file;
+    $update_src = sql_update("UPDATE `image` SET `Image_src` = '{$Image_src}' 
                               WHERE `Image_src` = '{$tmp_name_file}'");
     if (!$add_update || !$update_src){
         echo('Ошибка! Фотография не добавлена');
@@ -34,7 +35,7 @@ function Add_photo($image_info,$name_file,$tmp_name_file,$type_file,$upload_dir,
             if($_FILES["Image_src"]["size"] > $max_size) {
                 die("Размер файла не должен превышать 10 мегабайт.");
             }
-            if (move_uploaded_file($tmp_name_file, $upload_file)){
+            if (move_uploaded_file($tmp_name_file, '../'.$Image_src)){
                 echo('Фотография добавлена');
             }
             else {
@@ -50,35 +51,36 @@ function Add_photo($image_info,$name_file,$tmp_name_file,$type_file,$upload_dir,
     };
 }
 function Get_Id_Author($users_name,$type_user){
-    $id_author = sql_select("SELECT `id_users` FROM `users`
+    $id_author = sql_select("SELECT `id_user` FROM `users`
                             WHERE `users_name`='{$users_name}'
                             AND `type_user`='{$type_user}'");
-    return $id_author;
+    return mysqli_fetch_assoc($id_author)['id_user'];
 }
 function Get_Id_Properties_Image($width_image,$height_image){
-    $id_properties_image = sql_select("SELECT `id_properties_image` FROM `properties_images`
+    $id_properties_image = sql_select("SELECT `id_propertie` FROM `properties_images`
                                       WHERE `width_image`='{$width_image}'
                                       AND `height_image`='{$height_image}'");
-    if ($id_properties_image=false)
+    $id_properties_image = mysqli_fetch_assoc($id_properties_image);
+    if (!$id_properties_image)
     {
-        sql_update("INSERT INTO `properties_images` (`width_image`), (`height_image`) 
+        sql_update("INSERT INTO `properties_images` (`width_image`,`height_image`) 
                   VALUES ('{$width_image}', '{$height_image}')");
-        $id_properties_image = mysqli_fetch_assoc(sql_select("SELECT `id_properties_image` FROM `properties_images`
+        $id_properties_image = mysqli_fetch_assoc(sql_select("SELECT `id_propertie` FROM `properties_images`
                                                                     WHERE `width_image`='{$width_image}' 
                                                                     AND `height_image` = '{$height_image}'"));
     }
-    return $id_properties_image['id_properties_image'];
+    return $id_properties_image['id_propertie'];
 }
 function Get_Id_Event($Source,$type_data){
     $id_type = sql_select("SELECT `id_Type` FROM `type_of_data`
                           WHERE `type_data`='{$type_data}'");
-    if ($id_type = mysqli_fetch_assoc($id_type))
+    if ($id_type = mysqli_fetch_assoc($id_type)['id_Type'])
     {
         $result_id_event = sql_select("SELECT `id_event` FROM `event`
                                WHERE `Source`='{$Source}' 
-                               AND `Type_id_Tipe` = '{$id_type['id_type']}'");
+                               AND `Type_id_Type` = '{$id_type}'");
         if(!($result_id_event= mysqli_fetch_assoc($result_id_event))) {
-            sql_update("INSERT INTO `event` (`Source`), (`Type_id_Type`) 
+            sql_update("INSERT INTO `event` (`Source`, `Type_id_Type`) 
                       VALUES ('{$Source}', '{$id_type}')");
             $result_id_event = mysqli_fetch_assoc(sql_select("SELECT `id_event` FROM `event`
                                                             WHERE `Source`='{$Source}' 
